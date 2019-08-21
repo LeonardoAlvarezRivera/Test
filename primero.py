0834b6a2efc80536c2ca2576c5fb5f34e591f464
youilab <leonardoalvriv@gmail.com>
2019-08-21 12:46:38 -0500

    error para imprimir tres veces

---
+++ primero.py
@@ -0,0 +1,7 @@
+def printHola():
+    print("commit: Lo usas cuando terminas de editar")
+    print("push: Lo usas para subir el ¡ código")
+    print("pull: Descargar la última versión para poder editar")
+
+
+printHola()
